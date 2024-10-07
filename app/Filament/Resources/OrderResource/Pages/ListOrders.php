601abe13334d3d3754ad31426132bbf415e060ad
Minte <mintesinottamene0917@gmail.com>
2024-10-07 15:30:44 +0300

    ListOrders view

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\OrderResource\Pages;
 
-use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\OrderResource;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\OrderResource\Widgets\OrderSates;
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +18,33 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return[
+            OrderSates::class
+        ];
+    }
+
+    //if want display footer heading
+    // protected function getFooterWidgets(): array
+    // {
+    //     return[
+    //         OrderSates::class
+    //     ];
+    // }
+
+    public function getTabs(): array
+    {
+        return[
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status', 'new')),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+            'cancelled' => Tab::make()->query(fn ($query) => $query->where('status', 'cancelled')),
+        ];
+    }
+
+
 }
--- app/Filament/Resources/OrderResource/Widgets/OrderSates.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderSates.php
@@ -2,15 +2,20 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use App\Models\Order;
+use Illuminate\Support\Number;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class OrderSates extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('New Order', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
+            Stat::make('Average price', Number::currency(Order::query()->avg('grand_total'), 'INR')),
         ];
     }
 }
