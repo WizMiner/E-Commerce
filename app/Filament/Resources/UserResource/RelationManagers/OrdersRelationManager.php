1a48d7b8ceafaa65d239038eb79aeab291493554
Minte <mintesinottamene0917@gmail.com>
2024-10-07 16:34:02 +0300

    user order view finished

--- app/Filament/Resources/UserResource/RelationManagers/OrdersRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/OrdersRelationManager.php
@@ -3,12 +3,16 @@
 namespace App\Filament\Resources\UserResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use App\Models\Order;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use App\Filament\Resources\OrderResource;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class OrdersRelationManager extends RelationManager
 {
@@ -18,9 +22,7 @@ class OrdersRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('id')
-                    ->required()
-                    ->maxLength(255),
+                //
             ]);
     }
 
@@ -29,16 +31,37 @@ class OrdersRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('id')
             ->columns([
-                Tables\Columns\TextColumn::make('id'),
-            ])
+                TextColumn::make('id')->label('Order ID')->searchable(),
+
+                TextColumn::make('grand_total')->money('INR'),
+
+                TextColumn::make('status')->badge()->color(fn (string $state): string => match($state){
+                    'new' => 'info',
+                    'processing' => 'warning',
+                    'shipped' => 'success',
+                    'delivered' => 'success',
+                    'cancelled' => 'danger',
+                })
+                ->icon(fn (string $state): string => match($state){
+                    'new' => 'heroicon-m-sparkles',
+                    'processing' => 'heroicon-m-arrow-path',
+                    'shipped' => 'heroicon-m-truck',
+                    'delivered' => 'heroicon-m-check-badge',
+                    'cancelled' => 'heroicon-m-x-circle',
+                })->sortable(),
+
+                TextColumn::make('payment_method')->sortable()->searchable(),
+                TextColumn::make('payment_status')->sortable()->badge()->searchable(),
+                TextColumn::make('created_at')->label('Order Date')->dateTime(),
+                ])
             ->filters([
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Action::make('View Order')->url(fn(Order $record): string => OrderResource::getUrl('view', ['record' => $record]))->color('info')->icon('heroicon-o-eye'),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
