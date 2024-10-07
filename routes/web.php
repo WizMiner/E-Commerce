6c195fa643d43c04a84c527acbf59500d6d33a7d
Minte <mintesinottamene0917@gmail.com>
2024-10-07 23:18:27 +0300

    my Orders page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
@@ -14,4 +16,6 @@ Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
 Route::get('/products/{product}', ProductDetailPage::class);
 Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
 
