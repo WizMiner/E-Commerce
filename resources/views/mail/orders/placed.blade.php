159c6e6ac3ed0a59e2a7e475ec60dc09405ac32f
Minte <mintesinottamene0917@gmail.com>
2024-11-20 17:56:01 +0300

    email text

--- resources/views/mail/orders/placed.blade.php
+++ resources/views/mail/orders/placed.blade.php
@@ -1,12 +1,20 @@
 <x-mail::message>
-# Order placed successfully!
+# Order Placed Successfully!
 
-Thank you for your order. your order number is: {{ $order->id }}.
+Thank you for shopping with us! Your order number is **{{ $order->id }}**.
+
+We’re excited to process your order and get your items delivered to you as soon as possible.
+If you have any questions or need to modify your order, feel free to reach out to our support team.
 
 <x-mail::button :url="$url">
-View order
+View Order
 </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+Thanks again for choosing us!
+**{{ config('app.name') }}**
+
+Need help? Contact our support team at
+[**mintesinottamene0917@gmail.com**](mailto:support@ecommerce.com).
+
+© {{ now()->year }} {{ config('app.name') }}. All rights reserved.
 </x-mail::message>
